ccdd1a2d204f03fc2c774fdab583e478153b307f
asharkhangithub <onlyforashar12@gmail>
2025-02-11 23:29:52 +0500

    fix deployment error

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,8 +1,10 @@
 // Querying with "sanityFetch" will keep content automatically updated
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
-import { defineLive } from "next-sanity";
-import { client } from './client'
+// import { defineLive } from "next-sanity";
+// import { client } from './client'
+
+import { client } from "./sanity";
 
 export const { sanityFetch, SanityLive } = defineLive({ 
   client: client.withConfig({ 
@@ -11,3 +13,7 @@ export const { sanityFetch, SanityLive } = defineLive({
     apiVersion: 'vX' 
   }) 
 });
+function defineLive(arg0: { client: any; }): { sanityFetch: any; SanityLive: any; } {
+  throw new Error("Function not implemented.");
+}
+
